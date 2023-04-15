63c725841d5e3432bb48132c73b25f3cd58d4429
Rangwath <rangwath@gmail.com>
2023-04-15 20:01:00 +0200

    Add final scene ui canvas and menus

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,7 +5,7 @@ public class MenuManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("MVATestScene");
+        SceneManager.LoadScene("FinalScene");
     }
 
     public void LoadMainMenu()
