ecb567b639f6461a890a15df5c69ce9d821a0dfe
Rangwath <rangwath@gmail.com>
2023-04-15 20:41:50 +0200

    Add state changer automatic initialization

--- Assets/Scripts/StateChangerImplementation.cs
+++ Assets/Scripts/StateChangerImplementation.cs
@@ -5,6 +5,20 @@ public class StateChangerImplementation : StateChanger
     [SerializeField] private GameObject cleanObject;
     [SerializeField] private GameObject dirtyObject;
 
+    private void Start() 
+    {
+        if (isClean)
+        {
+            cleanObject.SetActive(true);
+            dirtyObject.SetActive(false);
+        }
+        else
+        {
+            dirtyObject.SetActive(true);
+            cleanObject.SetActive(false);
+        }
+    }
+
     public override void ChangeState()
     {
         Debug.Log("State changed on " + gameObject.name);
