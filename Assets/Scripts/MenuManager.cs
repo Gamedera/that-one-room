feeda3d8ff0a5d7114736d11872360c8b6dd2da3
Rangwath <rangwath@gmail.com>
2023-04-16 13:20:48 +0200

    Add starting dialogue screen

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,6 +5,11 @@ public class MenuManager : MonoBehaviour
 {
     public void StartGame()
     {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void RestartGame()
+    {
         SceneManager.LoadScene("FinalScene");
     }
 
