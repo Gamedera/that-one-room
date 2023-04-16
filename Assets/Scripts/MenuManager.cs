aa00e1e0a21ba98b6d7770c0524945892e69aa68
Andrea Kopecna <a.kopecna@seznam.cz>
2023-04-16 15:41:12 +0200

    Tune UI scenes

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,7 +5,7 @@ public class MenuManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("StartScene");
+        SceneManager.LoadScene("StartSceneMom");
     }
 
     public void RestartGame()
